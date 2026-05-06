283f33fed96432868420b9b1458e5ad595be3993
Richard Henrique <richardhenriquedeabreuferreira@gmail.com>
2026-05-06 15:51:36 -0300

    feat: implementa interações de menu lateral e filtros

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,1 +1,25 @@
 const filtrobtn = document.querySelector("#btn-filtro");
+const asideToggle = document.querySelector("#aside-toggle");
+
+asideToggle.addEventListener("click", () => {
+    const aside = document.querySelector(".aside-menu");
+    const icon1 = document.querySelector(".aside-icon-open");
+    const icon2 = document.querySelector(".aside-icon-fechado");
+
+    aside.classList.toggle("inativo")
+    icon1.classList.toggle("inativo")
+    icon2.classList.toggle("inativo")
+})
+
+filtrobtn.addEventListener("click", () => {
+    const containerFiltro = document.querySelector(".container-filtros");
+
+    filtrobtn.classList.toggle("click");
+    filtrobtn.classList.toggle("animar");
+    containerFiltro.classList.toggle('inativo');
+
+    filtrobtn.addEventListener("animationend", () => {
+        filtrobtn.classList.toggle("animar");
+    })
+
+})
